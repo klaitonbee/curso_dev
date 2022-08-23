980f959afc36671c0e5a3b390e3b9da32f1268f2
Klaiton Bee <klaitoncesarbee@gmail.com>
2022-08-23 20:54:46 -0300

    Uso de Variaveis

--- helloworld.js
+++ helloworld.js
@@ -3,10 +3,17 @@ console.log("Halo Dunia")
 console.log("Hello World")
 console.log("Olá Mundo")
 
-let minhavar //vou criar uma caixinha para guardar coisinhas
-let outravariavel = 1
+let variavel1 = "Klaiton Massa" //vou criar uma caixinha para guardar coisinhas
+console.log(variavel1)
 
-minhavar = "Klaiton Manda Bem" // estou guardando este valor na sua caixinha
+let variavel2 = "Klaiton Maneiro"
+console.log(variavel1)
+console.log(variavel2)
 
-console.log(minhavar)
-console.log(outravariavel)
+variavel1 = "Klaiton Agora é MUITO Massa"
+console.log(variavel1)
+console.log(variavel2)
+
+variavel2 = "Klaiton Agora é MUITO Maneiro"
+console.log(variavel1)
+console.log(variavel2)
