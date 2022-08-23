4f4c278a7d176ad525dcc39358528872e4668a96
Klaiton Bee <klaitoncesarbee@gmail.com>
2022-08-23 20:44:33 -0300

    Criando Variaveis

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,12 @@
+//Comentário do Código
 console.log("Halo Dunia")
 console.log("Hello World")
 console.log("Olá Mundo")
+
+let minhavar //vou criar uma caixinha para guardar coisinhas
+let outravariavel = 1
+
+minhavar = "Klaiton Manda Bem" // estou guardando este valor na sua caixinha
+
+console.log(minhavar)
+console.log(outravariavel)
